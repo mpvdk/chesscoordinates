bb98f90a17e65715dbe3f20cdbec28be2896dad7
martijn <martijnvdkerkhof@gmail.com>
2024-04-04 16:32:35 +0200

    remove emailcopy script from pages where it is not relevant and guard the addeventlistener with if statement

--- assets/js/EmailCopy.js
+++ assets/js/EmailCopy.js
@@ -1,20 +1,23 @@
-document.getElementById('emailbtn').addEventListener('click', function () {
-  if (navigator.clipboard) {
-    navigator.clipboard
-      .writeText('info@chesscoordinates.com')
-      .then(() => {
-        document.getElementById('toast').innerHTML = 'copied!</br>info@chesscoordinates.com';
-        document.getElementById('toast').classList.remove('toast-hidden');
-        setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
-      })
-      .catch(() => {
-        document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
-        document.getElementById('toast').classList.remove('toast-hidden');
-        setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
-      });
-  } else {
-    document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
-    document.getElementById('toast').classList.remove('toast-hidden');
-    setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
-  }
-});
+const emailBtn = document.getElementById('emailbtn');
+if (emailBtn) {
+  emailBtn.addEventListener('click', function () {
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText('info@chesscoordinates.com')
+        .then(() => {
+          document.getElementById('toast').innerHTML = 'copied!</br>info@chesscoordinates.com';
+          document.getElementById('toast').classList.remove('toast-hidden');
+          setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
+        })
+        .catch(() => {
+          document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
+          document.getElementById('toast').classList.remove('toast-hidden');
+          setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
+        });
+    } else {
+      document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
+      document.getElementById('toast').classList.remove('toast-hidden');
+      setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
+    }
+  });
+}
