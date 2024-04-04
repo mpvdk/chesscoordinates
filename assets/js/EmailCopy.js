91d8477284b27ab83f9a12a7ea510476e4f76ce1
martijn <martijnvdkerkhof@gmail.com>
2024-04-04 16:24:37 +0200

    some visual and logical updates

--- assets/js/EmailCopy.js
+++ assets/js/EmailCopy.js
@@ -1,14 +1,20 @@
 document.getElementById('emailbtn').addEventListener('click', function () {
-  navigator.clipboard
-    .writeText('info@chesscoordinates.com')
-    .then(() => {
-      document.getElementById('toast').innerHTML = 'copied!</br>info@chesscoordinates.com';
-      document.getElementById('toast').classList.remove('toast-hidden');
-      setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 3000);
-    })
-    .catch(() => {
-      document.getElementById('toast').innerHTML = 'failed to copy!</br>info@chesscoordinates.com';
-      document.getElementById('toast').classList.remove('toast-hidden');
-      setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 3000);
-    });
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText('info@chesscoordinates.com')
+      .then(() => {
+        document.getElementById('toast').innerHTML = 'copied!</br>info@chesscoordinates.com';
+        document.getElementById('toast').classList.remove('toast-hidden');
+        setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
+      })
+      .catch(() => {
+        document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
+        document.getElementById('toast').classList.remove('toast-hidden');
+        setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
+      });
+  } else {
+    document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
+    document.getElementById('toast').classList.remove('toast-hidden');
+    setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
+  }
 });
