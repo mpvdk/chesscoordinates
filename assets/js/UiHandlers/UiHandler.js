f204b71ca0472eec9013ae6c8f77dca4bdf432d7
martijn <martijnvdkerkhof@gmail.com>
2024-04-12 17:31:58 +0200

    add dark mode

--- assets/js/UiHandlers/UiHandler.js
+++ assets/js/UiHandlers/UiHandler.js
@@ -4,10 +4,32 @@ export default class UiHandler {
     this.els.navWrapperHeading = document.querySelector('.nav-wrapper-heading');
     this.els.modeNavigation = document.querySelector('.nav-wrapper nav');
     this.els.modeNavigationCloseButton = document.querySelector('.mobile-nav-close-button');
+    this.els.themeToggle = document.querySelector('.theme-selector-container');
 
     this.els.navWrapperHeading.addEventListener('click', this.modeSelectorClicked);
     this.els.modeNavigationCloseButton.addEventListener('click', this.closeNavigation);
+    this.els.themeToggle.addEventListener('click', this.toggleTheme);
+
+    this.setTheme();
   }
+
+  setTheme = () => {
+    let theme = window.localStorage.getItem('theme');
+
+    if (!theme) {
+      if (window.matchMedia) {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          theme = 'dark';
+        } else {
+          theme = 'light';
+        }
+      } else {
+        theme = 'light';
+      }
+    }
+
+    document.querySelector('html').dataset.theme = theme === 'dark' ? 'dark' : 'light';
+  };
 
   modeSelectorClicked = () => {
     this.els.modeNavigation.classList.add('active');
@@ -17,5 +39,22 @@ export default class UiHandler {
 
   closeNavigation = () => {
     this.els.modeNavigation.classList.remove('active');
+  };
+
+  toggleTheme = (e) => {
+    console.log('toggle');
+    const theme = window.localStorage.getItem('theme');
+    if (theme) {
+      if (theme === 'dark') {
+        window.localStorage.setItem('theme', 'light');
+        document.querySelector('html').dataset.theme = 'light';
+      } else {
+        window.localStorage.setItem('theme', 'dark');
+        document.querySelector('html').dataset.theme = 'dark';
+      }
+    } else {
+      window.localStorage.setItem('theme', 'light');
+      document.querySelector('html').dataset.theme = 'light';
+    }
   };
 }
