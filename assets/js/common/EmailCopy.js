fe8639eaf84c357b96474fbfe85a5153f0cd5dfa
martijn <martijnvdkerkhof@gmail.com>
2024-04-12 10:58:31 +0200

    make EmailCopy.js a module so it doesn't operate in global namespace

--- assets/js/common/EmailCopy.js
+++ assets/js/common/EmailCopy.js
@@ -1,25 +1,24 @@
-const emailBtns = document.querySelectorAll('.emailbtn');
-if (emailBtns) {
-  emailBtns.forEach((btn) =>
-    btn.addEventListener('click', function () {
-      if (navigator.clipboard) {
-        navigator.clipboard
-          .writeText('info@chesscoordinates.com')
-          .then(() => {
-            document.getElementById('toast').innerHTML = 'copied!</br>info@chesscoordinates.com';
-            document.getElementById('toast').classList.remove('toast-hidden');
-            setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
-          })
-          .catch(() => {
-            document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
-            document.getElementById('toast').classList.remove('toast-hidden');
-            setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
-          });
-      } else {
-        document.getElementById('toast').innerHTML = 'contact me at</br>info@chesscoordinates.com';
-        document.getElementById('toast').classList.remove('toast-hidden');
-        setTimeout(() => document.getElementById('toast').classList.add('toast-hidden'), 4000);
-      }
-    })
-  );
+const emailCopyJsEmailButton = document.querySelector('.emailbtn');
+const emailCopyJsToast = document.getElementById('toast');
+if (emailCopyJsEmailButton) {
+  emailCopyJsEmailButton.addEventListener('click', function () {
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText('info@chesscoordinates.com')
+        .then(() => {
+          emailCopyJsToast.innerHTML = 'copied!</br>info@chesscoordinates.com';
+          emailCopyJsToast.classList.remove('toast-hidden');
+          setTimeout(() => emailCopyJsToast.classList.add('toast-hidden'), 4000);
+        })
+        .catch(() => {
+          emailCopyJsToast.innerHTML = 'contact me at</br>info@chesscoordinates.com';
+          emailCopyJsToast.classList.remove('toast-hidden');
+          setTimeout(() => emailCopyJsToast.classList.add('toast-hidden'), 4000);
+        });
+    } else {
+      emailCopyJsToast.innerHTML = 'contact me at</br>info@chesscoordinates.com';
+      emailCopyJsToast.classList.remove('toast-hidden');
+      setTimeout(() => emailCopyJsToast.classList.add('toast-hidden'), 4000);
+    }
+  });
 }
